33f52aa308fdd98a82bef56831fa9066ea4f8901
ConfuzzedCat <ConfuzzedCat@ConfuzzedCat>
2022-02-25 02:47:08 +0100

    Fixed not moving all dlls and only sb's

--- Program.cs
+++ Program.cs
@@ -44,36 +44,47 @@ namespace StreamerBotUpdater
                 Directory.GetFiles(path, "coreclr.dll")[0],
                 Directory.GetFiles(path, "mscordaccore.dll")[0],
             };
-            if(String.Compare(latestVersion, sbVersion) == 0)
+            string[] sbDlls = new string[]
+            {
+                Path.Combine(path, "System.ValueTuple.dll"),
+                Path.Combine(path, "System.Threading.Tasks.Extensions.dll"),
+                Path.Combine(path, "System.Threading.Channels.dll"),
+                Path.Combine(path, "System.Text.Encoding.CodePages.dll"),
+                Path.Combine(path, "System.Security.Principal.Windows.dll"),
+                Path.Combine(path, "System.Security.AccessControl.dll"),
+                Path.Combine(path, "System.Runtime.CompilerServices.Unsafe.dll"),
+                Path.Combine(path, "System.Reflection.Metadata.dll"),
+                Path.Combine(path, "System.Numerics.Vectors.dll"),
+                Path.Combine(path, "System.Memory.dll"),
+                Path.Combine(path, "System.Collections.Immutable.dll"),
+                Path.Combine(path, "System.Buffers.dll"),
+                Path.Combine(path, "streamer.bot.png"),
+                Path.Combine(path, "Streamer.bot.pdb"),
+                Path.Combine(path, "Streamer.bot.exe.config"),
+                Path.Combine(path, "Streamer.bot.exe"),
+                Path.Combine(path, "Mono.Cecil.Rocks.dll"),
+                Path.Combine(path, "Mono.Cecil.Pdb.dll"),
+                Path.Combine(path, "Mono.Cecil.Mdb.dll"),
+                Path.Combine(path, "Mono.Cecil.dll"),
+                Path.Combine(path, "Microsoft.Win32.Registry.dll"),
+                Path.Combine(path, "Microsoft.Extensions.DependencyInjection.dll"),
+                Path.Combine(path, "Microsoft.Extensions.DependencyInjection.Abstractions.dll"),
+                Path.Combine(path, "Microsoft.CodeAnalysis.dll"),
+                Path.Combine(path, "Microsoft.CodeAnalysis.CSharp.dll"),
+                Path.Combine(path, "Microsoft.Bcl.AsyncInterfaces.dll"),
+            };
+            if (String.Compare(latestVersion, sbVersion) == 0)
             {
                 Console.WriteLine($"Streamer.Bot is already the latest version: {sbVersion}");
                 Environment.Exit(0);
             }
-            if (sbRunning && process != null) 
+            if (sbRunning && process != null)
             {
                 Console.WriteLine("Streamer.Bot is running... The updater will wait till it's close with continuing!");
-                process.WaitForExit(); 
+                process.WaitForExit();
             }
             Console.WriteLine($"Older version found: {sbVersion}. The latest version is {latestVersion}!");
-            Console.WriteLine("Backing up old files...");
-            string backupPath = Path.Combine(path, "OldVer_Backup");
-            if (!Directory.Exists(backupPath))
-            {
-                Directory.CreateDirectory(backupPath);
-            }
-            else
-            {
-                Directory.Delete(backupPath, true);
-            }
-            Directory.CreateDirectory(backupPath);
-            CopyFilesByExtension(path, backupPath, "*.dll", programDlls);
-            File.Copy("streamer.bot.png", Path.Combine(backupPath, "streamer.bot.png"), false);
-            if (File.Exists(Path.Combine(path, "streamer.bot.pdb")))
-            {
-                File.Copy("streamer.bot.pdb", Path.Combine(backupPath, "streamer.bot.pdb"), false);
-            }
-            File.Copy("streamer.bot.exe.config", Path.Combine(backupPath, "streamer.bot.exe.config"), false);
-            File.Copy("streamer.bot.exe", Path.Combine(backupPath, "streamer.bot.exe"), false);            
+            Console.WriteLine("Downloading new version and getting what files to backup.");
             using (WebClient wc = new WebClient())
             {
                 try
@@ -84,7 +95,28 @@ namespace StreamerBotUpdater
                     wc.DownloadFile(link, Path.Combine(_path, $"Streamer.bot-{latestVersion}.zip"));
                     if (File.Exists(Path.Combine(_path, $"Streamer.bot-{latestVersion}.zip")))
                     {
-                        ZipFile.ExtractToDirectory(Path.Combine(_path, $"Streamer.bot-{latestVersion}.zip"), path, true);
+
+                        ZipFile.ExtractToDirectory(Path.Combine(_path, $"Streamer.bot-{latestVersion}.zip"), _path, true);
+                        File.Delete(Path.Combine(_path, $"Streamer.bot-{latestVersion}.zip"));
+                        string[] sbNewFiles = Directory.GetFiles(_path);
+                        Console.WriteLine("Backing up old files...");
+                        string backupPath = Path.Combine(path, "OldVer_Backup");
+                        if (!Directory.Exists(backupPath))
+                        {
+                            Directory.CreateDirectory(backupPath);
+                        }
+                        else
+                        {
+                            Directory.Delete(backupPath, true);
+                            Directory.CreateDirectory(backupPath);
+                        }
+                        List<string> oldFilesList = new List<string>();
+                        foreach(string file in sbNewFiles)
+                        {
+                            oldFilesList.Add(Path.Combine(path, file.Remove(0, file.LastIndexOf('\\') + 1)));
+                        }
+                        CopyFiles(backupPath, oldFilesList.ToArray());
+                        CopyFiles(path, sbNewFiles);
                         sbVersion = FileVersionInfo.GetVersionInfo(Path.Combine(path, "Streamer.bot.exe")).FileVersion.Remove(5);
                         if (String.Compare(latestVersion, sbVersion) == 0)
                         {
@@ -98,23 +130,17 @@ namespace StreamerBotUpdater
                 {
                     Console.WriteLine(ex.ToString());
                     Console.ReadKey();
+                    Environment.Exit(0);
                 }
             }
+            
+            
         }
-        //static private void CopyFilesByExtension(string path, string destPath, string fileExtension, bool ow = true)
-        //{
-        //    string[] files = Directory.GetFiles(path, fileExtension);
-        //    foreach (string item in files)
-        //    {
-        //        File.Copy(item, Path.Combine(destPath, item.Remove(0, item.LastIndexOf('\\') + 1)), ow);
-        //    }
-        //}
-        static private void CopyFilesByExtension(string path, string destPath, string fileExtension, string[] exclude, bool ow = true)
+        static private void CopyFiles(string destPath, string[] files, bool ow = true)
         {
-            string[] files = Directory.GetFiles(path, fileExtension);
             foreach (string item in files)
             {
-                if(!exclude.Any(item.Contains))
+                if (File.Exists(item))
                 {
                     File.Copy(item, Path.Combine(destPath, item.Remove(0, item.LastIndexOf('\\') + 1)), ow);
                 }
